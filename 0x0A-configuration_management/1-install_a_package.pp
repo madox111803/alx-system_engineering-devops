03d82d992cf63864ecc58e94dd056b7e6ab91d01
madox111803 <madox111803@gmail.com>
2024-07-03 05:36:09 -0700

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,5 @@
-# Ensure python3-pip is installed
-package { 'python3-pip':
-  ensure => present,
-}
-
-# Ensure Flask version 2.1.0 is installed using pip3
-package { 'Flask':
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3-pip'],
+  provider => 'pip3'
 }
-
